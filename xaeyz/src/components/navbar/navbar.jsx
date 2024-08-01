2c26a4f21707c922d307b799540a9ff202157e59
Saurav <sauravgamer441@gmail.com>
2024-08-01 22:41:22 +0545

    +minor css changes - alignment issues unchecked

--- xaeyz/src/components/navbar/navbar.jsx
+++ xaeyz/src/components/navbar/navbar.jsx
@@ -9,7 +9,12 @@ export default function NavBar(){
          {/* <img className='globe-image'     src="https://www.freeiconspng.com/thumbs/global-icon/earth-global-world-icon--0.png" alt="an image" /> */}
             </div>
             <div className="main-text">
-              <h3>THE DOMICILE</h3>
+              <h3 className="heading">THE DOMICILE</h3>
+            </div>
+            <div className="subtext-1">
+                <h3>
+                {/* &gt;*/} FORGED BY REAUCTIONED 
+                </h3>
             </div>
 
 
