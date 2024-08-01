809a0e7729f901bfc6a9687de5514e2f8ee3f117
Saurav <sauravgamer441@gmail.com>
2024-08-01 22:01:30 +0545

    + custom font + noise css for the bg + img

---
+++ xaeyz/src/components/navbar/navbar.jsx
@@ -0,0 +1,18 @@
+import "./navbar.css"
+
+
+export default function NavBar(){
+
+    return(
+        <header className="navbar">
+            <div className="globe-wrapper">
+         {/* <img className='globe-image'     src="https://www.freeiconspng.com/thumbs/global-icon/earth-global-world-icon--0.png" alt="an image" /> */}
+            </div>
+            <div className="main-text">
+              <h3>THE DOMICILE</h3>
+            </div>
+
+
+        </header>
+    )
+}
